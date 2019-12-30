3a502efa7cec59a7644ec51a4ba71a76a736973e
TVMain <tveeder@live.com>
2019-12-30 11:45:33 -0600

    adding functionality to modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,19 @@
 // BLACKHAWK FRIENDS
 
 var friends = [{
-    "name": "Patrick",
+    "name": "Patrick Kane",
     "photo": "https://commons.wikimedia.org/wiki/File:Patrick_Kane_-_Chicago_Blackhawks.jpg",
     "scores": ['3', '3', '3', '3', '3', '3', '3', '3', '3', '3']
 }, {
-    "name": "Jonathan",
+    "name": "Jonathan Toews",
     "photo": "https://commons.wikimedia.org/wiki/File:Jonathan_Toews_(5441820205).jpg",
     "scores": ['2', '2', '4', '5', '2', '2', '5', '3', '1', '2']
 }, {
-    "name": "Brandon",
+    "name": "Brandon Saad",
     "photo": 'https://commons.wikimedia.org/wiki/File:Brandon_Saad_Hogs.jpg',
     "scores": ['4', '2', '1', '5', '3', '1', '3', '2', '4', '5']
 }, {
-    "name": "Brent",
+    "name": "Brent Seabrook",
     "photo": 'https://commons.wikimedia.org/wiki/File:Brent_Seabrook_(23501039815)_(2).jpg',
     "scores": ['1', '3', '5', '5', '2', '2', '3', '4', '1', '5']
 }];
