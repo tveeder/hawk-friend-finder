38b49827aec9da968fadf84b5b0fc04937e19bfe
TVMain <tveeder@live.com>
2020-01-02 14:40:38 -0600

    adding to modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,31 @@
 // BLACKHAWK FRIENDS
 
-var friends = [{
-    "name": "Patrick Kane",
-    "photo": "https://commons.wikimedia.org/wiki/File:Patrick_Kane_-_Chicago_Blackhawks.jpg",
-    "scores": ['3', '3', '3', '3', '3', '3', '3', '3', '3', '3']
-}, {
-    "name": "Jonathan Toews",
-    "photo": "https://commons.wikimedia.org/wiki/File:Jonathan_Toews_(5441820205).jpg",
-    "scores": ['2', '2', '4', '5', '2', '2', '5', '3', '1', '2']
-}, {
-    "name": "Brandon Saad",
-    "photo": 'https://commons.wikimedia.org/wiki/File:Brandon_Saad_Hogs.jpg',
-    "scores": ['4', '2', '1', '5', '3', '1', '3', '2', '4', '5']
-}, {
-    "name": "Brent Seabrook",
-    "photo": 'https://commons.wikimedia.org/wiki/File:Brent_Seabrook_(23501039815)_(2).jpg',
-    "scores": ['1', '3', '5', '5', '2', '2', '3', '4', '1', '5']
-}];
+var friends = [
+	{
+		name: "Patrick",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/7/76/Patrick_Kane_-_Chicago_Blackhawks.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Jonathan",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/a/a5/Jonathan_Toews.JPG",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Corey",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Corey_Crawford_%285442402202%29_%28cropped1%29.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Andrew",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/5/54/Andrew_Shaw_%288397647%29.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Brandon",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/d/d2/Brandon_Saad_-_Chicago_Blackhawks_%28cropped%29.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
 
 module.exports = friends;
